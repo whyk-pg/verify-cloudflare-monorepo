784f35b097b0fb289f69143500d738a66966ab58
WhyK <windchimeyk@gmail.com>
2025-03-31 00:39:54 +0900

    feat: init project

---
+++ client/vite.config.ts
@@ -0,0 +1,42 @@
+import {
+  cloudflareDevProxyVitePlugin,
+  vitePlugin as remix,
+} from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { getLoadContext } from "./load-context";
+
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    cloudflareDevProxyVitePlugin({
+      getLoadContext,
+    }),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ["workerd", "worker", "browser"],
+    },
+  },
+  resolve: {
+    mainFields: ["browser", "module", "main"],
+  },
+  build: {
+    minify: true,
+  },
+});
